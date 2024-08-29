61f2d1b9819538ae2276f557c26248691fe6f3bd
johndoknjas <jdoknjas@sfu.ca>
2024-08-28 18:17:02 -0700

    print better

--- stockfish/models.py
+++ stockfish/models.py
@@ -360,8 +360,6 @@ class Stockfish:
             return
         self._prepare_for_new_position(False)
         for move in moves:
-            # if not self.is_move_correct(move):
-            #    raise ValueError(f"Cannot make move: {move}")
             self._put(f"position fen {self.get_fen_position()} moves {move}")
 
     def get_board_visual(self, perspective_white: bool = True) -> str:
@@ -611,7 +609,7 @@ class Stockfish:
 
         lines: List[str] = self._get_sf_go_command_output()
         for line in lines:
-            print(" ".join(line))
+            print(line)
         self.info = lines[-2]
         last_line_split = lines[-1].split(" ")
         return None if last_line_split[1] == "(none)" else last_line_split[1]
