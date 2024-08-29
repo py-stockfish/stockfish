b85162d3ecd0661576f46abca63d39c0befb53ec
johndoknjas <jdoknjas@sfu.ca>
2024-08-29 15:07:01 -0700

    format with black

--- tests/stockfish/test_models.py
+++ tests/stockfish/test_models.py
@@ -6,6 +6,7 @@ import subprocess
 from subprocess import Popen
 import os
 
+
 def send_command(process: Popen, command):
     process.stdin.write(command + "\n")
     process.stdin.flush()
@@ -14,11 +15,12 @@ def send_command(process: Popen, command):
         line = process.stdout.readline()
         lines.append(line)
         print(line)
-        if line in ('', '\n'):
+        if line in ("", "\n"):
             break
-        if any(x in line for x in ('bestmove', 'isready', 'readyok', 'uciok')):
+        if any(x in line for x in ("bestmove", "isready", "readyok", "uciok")):
             break
     return lines
+
 
 class TestStockfish:
 
@@ -35,18 +37,33 @@ class TestStockfish:
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
-            text=True
+            text=True,
         )
 
         send_command(process, "uci")
         send_command(process, "isready")
-        send_command(process, "position fen rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-        send_command(process, "position fen rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1")
-        send_command(process, "position fen rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
+        send_command(
+            process,
+            "position fen rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        )
+        send_command(
+            process,
+            "position fen rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1",
+        )
+        send_command(
+            process,
+            "position fen rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",
+        )
         wtime_lines = send_command(process, "go wtime 1000")
         btime_lines = send_command(process, "go btime 1000")
         send_command(process, "quit")
-        assert any(x.startswith(f'bestmove {move}') for move in ('d2d4', 'b1c3') for x in wtime_lines)
-        assert any(x.startswith(f'bestmove {move}') for move in ('d2d4', 'b1c3') for x in btime_lines)
-
-
+        assert any(
+            x.startswith(f"bestmove {move}")
+            for move in ("d2d4", "b1c3")
+            for x in wtime_lines
+        )
+        assert any(
+            x.startswith(f"bestmove {move}")
+            for move in ("d2d4", "b1c3")
+            for x in btime_lines
+        )
