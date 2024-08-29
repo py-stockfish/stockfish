97b135f02157968ce626158839ea5d67c6274fca
johndoknjas <jdoknjas@sfu.ca>
2024-08-29 15:05:16 -0700

    set position fixed

--- tests/stockfish/test_models.py
+++ tests/stockfish/test_models.py
@@ -42,7 +42,7 @@ class TestStockfish:
         send_command(process, "isready")
         send_command(process, "position fen rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
         send_command(process, "position fen rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1")
-        send_command(process, "position fen rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1")
+        send_command(process, "position fen rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
         wtime_lines = send_command(process, "go wtime 1000")
         btime_lines = send_command(process, "go btime 1000")
         send_command(process, "quit")
