9781082e73a4d68fa62781edf611ddd4aca72ca0
johndoknjas <jdoknjas@sfu.ca>
2026-01-01 19:52:35 -0800

    Release 4.0.5.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     name="stockfish",
     author="Ilya Zhelyabuzhsky",
     author_email="zhelyabuzhsky@icloud.com",
-    version="4.0.4",
+    version="4.0.5",
     license="MIT",
     keywords="chess stockfish",
     python_requires=">=3.10",
